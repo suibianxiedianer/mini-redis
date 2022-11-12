9e23738dcfb8208daaff2924f9ddfb72f3b26710
Jia Chao <jiachao2130@126.com>
2022-11-12 15:07:40 +0800

    finish: 添加 server 测试用例，订阅模式测试完成
    Signed-off-by: Jia Chao <jiachao2130@126.com>

--- tests/server.rs
+++ tests/server.rs
@@ -86,6 +86,7 @@ async fn key_value_timeout() {
     get_null(&mut stream).await;
 }
 
+/// 简单的发布/订阅测试
 #[tokio::test]
 async fn pub_sub() {
     let addr = start_server().await;
@@ -218,6 +219,171 @@ async fn pub_sub() {
                $3\r\nfoo\r\n\
                $3\r\nbar\r\n",
                &response);
+}
+
+/// 订阅管理测试
+#[tokio::test]
+async fn manage_subscription() {
+    let addr = start_server().await;
+
+    let mut publisher = TcpStream::connect(addr).await.unwrap();
+
+    // sub 订阅 `hello`
+    let mut sub = TcpStream::connect(addr).await.unwrap();
+    sub.write_all(b"*2\r\n\
+                     $9\r\nSUBSCRIBE\r\n\
+                     $5\r\nhello\r\n")
+        .await
+        .unwrap();
+
+    let mut response = [0; 34];
+    sub.read_exact(&mut response).await.unwrap();
+    assert_eq!(b"*3\r\n\
+               $9\r\nsubscribe\r\n\
+               $5\r\nhello\r\n\
+               :1\r\n",
+               &response);
+
+    // sub 订阅 `foo`
+    sub.write_all(b"*2\r\n\
+                     $9\r\nSUBSCRIBE\r\n\
+                     $3\r\nfoo\r\n")
+        .await
+        .unwrap();
+
+    let mut response = [0; 32];
+    sub.read_exact(&mut response).await.unwrap();
+    assert_eq!(b"*3\r\n\
+               $9\r\nsubscribe\r\n\
+               $3\r\nfoo\r\n\
+               :2\r\n",
+               &response);
+
+    // sub 取消 `hello` 订阅
+    sub.write_all(b"*2\r\n\
+                     $11\r\nUNSUBSCRIBE\r\n\
+                     $5\r\nhello\r\n")
+        .await
+        .unwrap();
+
+    let mut response = [0; 37];
+    sub.read_exact(&mut response).await.unwrap();
+    assert_eq!(b"*3\r\n\
+               $11\r\nunsubscribe\r\n\
+               $5\r\nhello\r\n\
+               :1\r\n",
+               &response);
+
+    // publisher 向 `hello`、`foo` 发送消息
+    publisher.write_all(b"*3\r\n\
+                        $7\r\nPUBLISH\r\n\
+                        $5\r\nhello\r\n\
+                        $5\r\nworld\r\n")
+        .await
+        .unwrap();
+
+    let mut response = [0; 4];
+    publisher.read_exact(&mut response).await.unwrap();
+    assert_eq!(b":0\r\n", &response);
+
+    publisher.write_all(b"*3\r\n\
+                        $7\r\nPUBLISH\r\n\
+                        $3\r\nfoo\r\n\
+                        $3\r\nbar\r\n")
+        .await
+        .unwrap();
+
+    publisher.read_exact(&mut response).await.unwrap();
+    assert_eq!(b":1\r\n", &response);
+
+    // sub 只能读取 `foo` 的消息
+    let mut response = [0; 35];
+    sub.read_exact(&mut response).await.unwrap();
+    assert_eq!(b"*3\r\n\
+               $7\r\nmessage\r\n\
+               $3\r\nfoo\r\n\
+               $3\r\nbar\r\n",
+               &response);
+
+    time::timeout(Duration::from_millis(100), sub.read(&mut response))
+        .await
+        .unwrap_err();
+
+    // 取消所有订阅
+    sub.write_all(b"*1\r\n$11\r\nUNSUBSCRIBE\r\n")
+        .await
+        .unwrap();
+
+    let mut response = [0; 35];
+    sub.read_exact(&mut response).await.unwrap();
+    assert_eq!(b"*3\r\n\
+               $11\r\nunsubscribe\r\n\
+               $3\r\nfoo\r\n\
+               :0\r\n",
+               &response);
+}
+
+/// 错误命令格式测试
+#[tokio::test]
+async fn send_error_unknown_command() {
+    let addr = start_server().await;
+//    let addr = "127.0.0.1:6379";
+
+    let mut stream = TcpStream::connect(addr).await.unwrap();
+
+    stream.write_all(b"*2\r\n\
+                     $3\r\nFOO\r\n\
+                     $5\r\nhello\r\n")
+        .await
+        .unwrap();
+
+    let mut response = [0; 29];
+    stream.read_exact(&mut response).await.unwrap();
+    assert_eq!(b"-Err: unknown command \'foo\'\r\n", &response);
+}
+
+/// 订阅模式只接收订阅相关命令
+#[tokio::test]
+async fn send_error_get_set_after_subscribe() {
+    let addr = start_server().await;
+
+    let mut stream = TcpStream::connect(addr).await.unwrap();
+
+    // 进入订阅模式
+    stream.write_all(b"*2\r\n\
+                     $9\r\nSUBSCRIBE\r\n\
+                     $5\r\nhello\r\n")
+        .await
+        .unwrap();
+
+    let mut response = [0; 34];
+    stream.read_exact(&mut response).await.unwrap();
+    assert_eq!(b"*3\r\n\
+               $9\r\nsubscribe\r\n\
+               $5\r\nhello\r\n\
+               :1\r\n",
+               &response);
+
+    stream.write_all(b"*3\r\n\
+                     $3\r\nSET\r\n\
+                     $5\r\nhello\r\n\
+                     $5\r\nworld\r\n")
+        .await
+        .unwrap();
+
+    let mut response = [0; 29];
+    stream.read_exact(&mut response).await.unwrap();
+    assert_eq!(b"-Err: unknown command \'set\'\r\n", &response);
+
+    stream.write_all(b"*2\r\n\
+                     $3\r\nGET\r\n\
+                     $5\r\nhello\r\n")
+        .await
+        .unwrap();
+
+    let mut response = [0; 29];
+    stream.read_exact(&mut response).await.unwrap();
+    assert_eq!(b"-Err: unknown command \'get\'\r\n", &response);
 }
 
 async fn get_ok(stream: &mut TcpStream) {
